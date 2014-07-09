f1420f4ebeccf3df9249b6949f015b89b906d916
Mario Solomou <webmaster@masodev.com>
2014-07-09 12:57:57 +0100

    Forms Creating and Partials
    DRY method applied and integrated some more bootstrap layouts and
    buttons.

--- crowdfund/app/controllers/projects_controller.rb
+++ crowdfund/app/controllers/projects_controller.rb
@@ -1,20 +1,39 @@
 class ProjectsController < ApplicationController
 
   def index
-   @projects = Project.all
+     @projects = Project.all
   end
 
   def show
-   @project = Project.find_by(params[:id])
+    @project = Project.find(params[:id])
   end
 
   def edit
-  	@project = Project.find_by(params[:id])
+    @project = Project.find(params[:id])
   end
 
   def update
-  	@project = Project.find_by(params[:id])
-  	@project.update(params[:project].permit(:name, :description, :target_pledge_amount, :pledging_ends_on))
+  	@project = Project.find(params[:id])
+  	@project.update(project_params)
   	redirect_to @project
   end
+
+  def new 
+  	@project = Project.new
+  end
+
+  def create
+   @project = Project.new(project_params)
+   @project.save
+   redirect_to @project
+  end
+
+private
+
+def project_params
+  params.require(:project).permit(:name, :description, :target_pledge_amount, :pledging_ends_on)
+  end
+
+
+
 end
